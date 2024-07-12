559e5505e6f09bf38e0b1cf7040657f4ca96f7de
ziani96 <81708016+ziani96@users.noreply.github.com>
2024-07-12 15:00:21 +0100

    Added post, author and tag tables

--- blog/views.py
+++ blog/views.py
@@ -1,12 +1,77 @@
 from django.shortcuts import render
+from datetime import date
+
+
+all_posts = [
+    {
+    "slug": "Hike in the mountains",
+    "image": "mountains.jpg",
+    "author": "Zaid",
+    "date": date(2024,7,10),
+    "title": "Mountain Hiking",
+    "excerpt": "There is nothing like the views you get when hiking in the mountains!",
+    "content": """Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla debitis labore obcaecati repellat laudantium similique, saepe molestias eum. Laborum, dolor id. Nam hic odit soluta ut delectus pariatur a repellat.
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla debitis labore obcaecati repellat laudantium similique, saepe molestias eum. Laborum, dolor id. Nam hic odit soluta ut delectus pariatur a repellat.
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla debitis labore obcaecati repellat laudantium similique, saepe molestias eum. Laborum, dolor id. Nam hic odit soluta ut delectus pariatur a repellat."""
+    },
+    {
+        "slug": "programming-is-fun",
+        "image": "coding.jpg",
+        "author": "Maximilian",
+        "date": date(2024, 3, 10),
+        "title": "Programming Is Great!",
+        "excerpt": "Did you ever spend hours searching that one error in your code? Yep - that's what happened to me yesterday...",
+        "content": """
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis nobis
+          aperiam est praesentium, quos iste consequuntur omnis exercitationem quam
+          velit labore vero culpa ad mollitia? Quis architecto ipsam nemo. Odio.
+
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis nobis
+          aperiam est praesentium, quos iste consequuntur omnis exercitationem quam
+          velit labore vero culpa ad mollitia? Quis architecto ipsam nemo. Odio.
+
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis nobis
+          aperiam est praesentium, quos iste consequuntur omnis exercitationem quam
+          velit labore vero culpa ad mollitia? Quis architecto ipsam nemo. Odio.
+        """
+    },
+    {
+        "slug": "into-the-woods",
+        "image": "woods.jpg",
+        "author": "Maximilian",
+        "date": date(2024, 8, 5),
+        "title": "Nature At Its Best",
+        "excerpt": "Nature is amazing! The amount of inspiration I get when walking in nature is incredible!",
+        "content": """
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis nobis
+          aperiam est praesentium, quos iste consequuntur omnis exercitationem quam
+          velit labore vero culpa ad mollitia? Quis architecto ipsam nemo. Odio.
+
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis nobis
+          aperiam est praesentium, quos iste consequuntur omnis exercitationem quam
+          velit labore vero culpa ad mollitia? Quis architecto ipsam nemo. Odio.
+
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis nobis
+          aperiam est praesentium, quos iste consequuntur omnis exercitationem quam
+          velit labore vero culpa ad mollitia? Quis architecto ipsam nemo. Odio.
+        """
+    }
+]
+
+def get_date(post):
+    return post['date']
+
 
 def starting_page(request):
-    return render(request,"blog/index.html")
+    sorted_posts = sorted(all_posts, key=get_date)
+    latest_posts = sorted_posts[-3:]
+    return render(request,"blog/index.html", {"posts": latest_posts})
 
 
 def posts(request):
-    return render(request, "blog/all-posts.html")
+    return render(request, "blog/all-posts.html", {"all_posts": all_posts})
 
 
 def post_detail(request, slug):
-    return render(request, "blog/post-detail.html", )
+    identified_post = next(post for post in all_posts if post['slug'] == slug)
+    return render(request, "blog/post-detail.html", {"post": identified_post} )
