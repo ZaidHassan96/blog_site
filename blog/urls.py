cab4353c603e7ad64e0669b20526652ac5a48a53
ziani96 <81708016+ziani96@users.noreply.github.com>
2024-07-17 17:00:10 +0100

    changing funtion based views to class based views

--- blog/urls.py
+++ blog/urls.py
@@ -4,7 +4,7 @@ from . import views
 
 
 urlpatterns = [
-    path("", views.starting_page, name="starting-page" ),
-    path("posts", views.posts, name="posts-page"),
-    path("posts/<slug>", views.post_detail, name="post-detail-page")
+    path("", views.StartingPageView.as_view(), name="starting-page" ),
+    path("posts", views.PostsView.as_view(), name="posts-page"),
+    path("posts/<slug>", views.PostDetailView.as_view(), name="post-detail-page")
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,77 +1,59 @@
-from django.shortcuts import render
+from typing import Any
+from django.db.models.query import QuerySet
+from django.shortcuts import render, get_object_or_404
 from datetime import date
+from django.views.generic import ListView
+from django.views.generic.detail import DetailView
+
+from .models import Post
 
 
-all_posts = [
-    {
-    "slug": "Hike in the mountains",
-    "image": "mountains.jpg",
-    "author": "Zaid",
-    "date": date(2024,7,10),
-    "title": "Mountain Hiking",
-    "excerpt": "There is nothing like the views you get when hiking in the mountains!",
-    "content": """Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla debitis labore obcaecati repellat laudantium similique, saepe molestias eum. Laborum, dolor id. Nam hic odit soluta ut delectus pariatur a repellat.
-    Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla debitis labore obcaecati repellat laudantium similique, saepe molestias eum. Laborum, dolor id. Nam hic odit soluta ut delectus pariatur a repellat.
-    Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla debitis labore obcaecati repellat laudantium similique, saepe molestias eum. Laborum, dolor id. Nam hic odit soluta ut delectus pariatur a repellat."""
-    },
-    {
-        "slug": "programming-is-fun",
-        "image": "coding.jpg",
-        "author": "Maximilian",
-        "date": date(2024, 3, 10),
-        "title": "Programming Is Great!",
-        "excerpt": "Did you ever spend hours searching that one error in your code? Yep - that's what happened to me yesterday...",
-        "content": """
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis nobis
-          aperiam est praesentium, quos iste consequuntur omnis exercitationem quam
-          velit labore vero culpa ad mollitia? Quis architecto ipsam nemo. Odio.
-
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis nobis
-          aperiam est praesentium, quos iste consequuntur omnis exercitationem quam
-          velit labore vero culpa ad mollitia? Quis architecto ipsam nemo. Odio.
-
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis nobis
-          aperiam est praesentium, quos iste consequuntur omnis exercitationem quam
-          velit labore vero culpa ad mollitia? Quis architecto ipsam nemo. Odio.
-        """
-    },
-    {
-        "slug": "into-the-woods",
-        "image": "woods.jpg",
-        "author": "Maximilian",
-        "date": date(2024, 8, 5),
-        "title": "Nature At Its Best",
-        "excerpt": "Nature is amazing! The amount of inspiration I get when walking in nature is incredible!",
-        "content": """
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis nobis
-          aperiam est praesentium, quos iste consequuntur omnis exercitationem quam
-          velit labore vero culpa ad mollitia? Quis architecto ipsam nemo. Odio.
-
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis nobis
-          aperiam est praesentium, quos iste consequuntur omnis exercitationem quam
-          velit labore vero culpa ad mollitia? Quis architecto ipsam nemo. Odio.
-
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis nobis
-          aperiam est praesentium, quos iste consequuntur omnis exercitationem quam
-          velit labore vero culpa ad mollitia? Quis architecto ipsam nemo. Odio.
-        """
-    }
-]
 
 def get_date(post):
     return post['date']
 
 
-def starting_page(request):
-    sorted_posts = sorted(all_posts, key=get_date)
-    latest_posts = sorted_posts[-3:]
-    return render(request,"blog/index.html", {"posts": latest_posts})
+class StartingPageView(ListView):
+    template_name = "blog/index.html"
+    model = Post
+    ordering = ["-date"]
+    context_object_name = "posts"
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        data = queryset[:3]
+        return data
 
 
-def posts(request):
-    return render(request, "blog/all-posts.html", {"all_posts": all_posts})
+# def starting_page(request):
+#     latest_posts = Post.objects.all().order_by("-date")[:3]
+ 
+#     return render(request,"blog/index.html", {"posts": latest_posts})
+
+class PostsView(ListView):
+    template_name = "blog/all-posts.html"
+    model = Post
+    ordering = ["-date"]
+    context_object_name = "all_posts"
+
+   
+        
+# def posts(request):
+#     all_posts = Post.objects.all().order_by("-date")
+#     return render(request, "blog/all-posts.html", {"all_posts": all_posts})
 
 
-def post_detail(request, slug):
-    identified_post = next(post for post in all_posts if post['slug'] == slug)
-    return render(request, "blog/post-detail.html", {"post": identified_post} )
+
+class PostDetailView(DetailView):
+    template_name = "blog/post-detail.html"
+    model = Post
+
+    def get_context_data(self, **kwargs: Any):
+        context =  super().get_context_data(**kwargs)
+        context["post_tags"] = self.object.tags.all()
+        return context
+    
+# def post_detail(request, slug):
+#     identified_post = get_object_or_404(Post, slug=slug)
+#     return render(request, "blog/post-detail.html", {"post": identified_post,
+#      "post_tags":identified_post.tags.all()})
