e3c5230f063713f5810eac5f7f64bc536ba5faef
ziani96 <81708016+ziani96@users.noreply.github.com>
2024-07-24 19:22:28 +0100

    Adding tests for models

--- blog/models.py
+++ blog/models.py
@@ -20,6 +20,10 @@ class Author(models.Model):
 
     def __str__(self):
         return self.full_name()
+    
+
+
+
        
 
 class Post(models.Model):
@@ -31,6 +35,18 @@ class Post(models.Model):
     content = models.TextField(validators=[MinLengthValidator(10)])
     author = models.ForeignKey(Author, on_delete=models.SET_NULL, null=True)
     tags = models.ManyToManyField(Tag)
+
+    def __str__(self):
+        return self.title
+
+
+class Comment(models.Model):
+    user_name = models.CharField(max_length=120)
+    user_email = models.EmailField()
+    text = models.TextField(max_length=400)
+    date = models.DateField(auto_now=True)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name="comments")
+
 
 
     
--- blog/tests.py
+++ blog/tests.py
@@ -1,3 +1,48 @@
 from django.test import TestCase
+from .models import Post, Author, Tag, Comment
+from django.utils.text import slugify
+from django.core.exceptions import ValidationError  # Import ValidationError
 
-# Create your tests here.
+
+class PostModelTestCase(TestCase):
+    
+    def setUp(self):
+        self.tag = Tag.objects.create(caption="TestTag")
+        self.author = Author.objects.create(first_name = "max", last_name = "davis", email_address = "max1@hotmail.com")
+        self.post = Post.objects.create(
+            title="Test Title",
+            excerpt="Test Excerpt",
+            content="This is a test content with more than ten characters.",
+            author=self.author
+        )
+        self.post.tags.add(self.tag)
+
+    def test_excerpt_length(self):
+         post = Post(title='Valid Title', excerpt='x' * 201, image = "hello", slug = slugify("Valid Title"), content='Valid content', author=self.author)
+         with self.assertRaises(ValidationError):
+              post.full_clean()
+    
+    def test_post_creation(self):
+        self.assertTrue(isinstance(self.post, Post  ))
+        self.assertEqual(self.post.__str__(), self.post.title)
+
+    def test_min_length_validator(self):
+       
+            post_with_short_content= Post.objects.create(
+                title="Another Title",
+                excerpt="Another Excerpt",
+                content="Too",
+                author=self.author,
+                slug=slugify("Another Title")
+            )
+
+            with self.assertRaises(ValidationError) as cm:
+                 post_with_short_content.full_clean()  
+        
+            self.assertIn('content', cm.exception.message_dict)
+        
+
+
+
+
+
