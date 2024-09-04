09b40168ccb55304d098925487a5c0bd523a5eae
ziani96 <81708016+ziani96@users.noreply.github.com>
2024-09-04 21:40:07 +0100

    commit

--- myenv/bin/activate.fish
+++ myenv/bin/activate.fish
@@ -33,7 +33,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/Users/zicoo/site_project/my_site/myenv"
+set -gx VIRTUAL_ENV "/Users/zicoo/blog_site/myenv"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
