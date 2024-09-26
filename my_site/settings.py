80230433eb2aa38eb954f59e86743ae85b6f9a06
ziani96 <81708016+ziani96@users.noreply.github.com>
2024-09-26 19:32:49 +0100

    blog site deployed via aws elasticbeanstalk

--- my_site/settings.py
+++ my_site/settings.py
@@ -13,8 +13,13 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 from pathlib import Path
 from dotenv import load_dotenv
 import os
-from pathlib import Path
+from os import getenv
+import environ
 
+env_path = Path('.') / '.env'
+load_dotenv(dotenv_path=env_path,override=True)
+env = environ.Env()
+environ.Env.read_env()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -24,24 +29,35 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-5c&vdw9h8=t5+@p$gp+@d*s205y4wpkk_c1$q^j#v2ajiik3@8'
+SECRET_KEY = os.getenv("SECRET_KEY")
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv("IS_DEVELOPMENT", "True") == "True" 
+print(DEBUG) # Converts the string to a boolean
 
-ALLOWED_HOSTS = []
 
+  # Reading .env file
+
+# Set ALLOWED_HOSTS
+ALLOWED_HOSTS = [
+    getenv("APP_HOST"),
+    '127.0.0.1'
+]
+# ALLOWED_HOSTS += ['127.0.0.1', 'localhost']
 
 # Application definition
 
 INSTALLED_APPS = [
+    'blog',
+    'storages',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-    'django.contrib.staticfiles',
-    'blog'
+    'django.contrib.staticfiles'
+    
 ]
 
 MIDDLEWARE = [
@@ -82,10 +98,15 @@ WSGI_APPLICATION = 'my_site.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',
+        'USER' : 'djangoblog' ,
+        'PASSWORD': os.getenv('DATABASE_PASSWORD'),
+        'HOST' : os.getenv('DATABASE_HOST'),
+        'PORT' : '5432'
     }
 }
+
 
 
 # Password validation
@@ -121,11 +142,11 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
-
-STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / "staticfiles"
+STATIC_URL = "/static/"
 
 STATICFILES_DIRS = [
-    BASE_DIR / "static"
+    BASE_DIR / "static",
 ]
 
 # Default primary key field type
@@ -136,19 +157,70 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 MEDIA_ROOT = BASE_DIR / "uploads"
 MEDIA_URL = "/files/"
 
+# if DEBUG:  # Local development
+#     STATIC_URL = '/static/'
+#     STATICFILES_DIRS = [BASE_DIR / "static"]
+#     STATIC_ROOT = BASE_DIR / "staticfiles"
+#     MEDIA_ROOT = BASE_DIR / "uploads"
+#     MEDIA_URL = "/files/"
+# else:  # Production
+#     AWS_STORAGE_BUCKET_NAME = "django-tech-blog"
+#     AWS_S3_REGION_NAME = "eu-west-2"
+#     AWS_ACCESS_KEY_ID = os.getenv('AWS_ACCESS_KEY_ID')
+#     AWS_SECRET_ACCESS_KEY = os.getenv('AWS_SECRET_ACCESS_KEY')
+
+#     STORAGES = {
+#         "default": {
+#         "BACKEND": "custom_storages.MediaFileStorage",
+#         },
+#         "staticfiles": {
+#         "BACKEND": "custom_storages.StaticFileStorage",
+#         }
+#     }
+#     AWS_S3_CUSTOM_DOMAIN = f"{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com"
+
+#     STATICFILES_FOLDER = "static"
+
+#     MEDIAFILES_FOLDER = "media"
 
 
-env_path = Path('.') / '.env'
-load_dotenv(dotenv_path=env_path,override=True)
 
-print("EMAIL_HOST_USER:", os.getenv('EMAIL_HOST_USER'))
-print("DEFAULT_FROM_EMAIL:", os.getenv('DEFAULT_FROM_EMAIL'))
-print(f"Loading .env file from: {env_path}") 
+
+
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.office365.com'
+EMAIL_HOST = 'smtp.sendgrid.net'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
-EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
-DEFAULT_FROM_EMAIL = os.getenv('DEFAULT_FROM_EMAIL')
+
+#SendGrid username and API key
+EMAIL_HOST_USER = 'apikey'  # This should always be 'apikey' for SendGrid
+EMAIL_HOST_PASSWORD = os.getenv('SENDGRID_API_KEY')  
+
+# Default from email
+DEFAULT_FROM_EMAIL = 'techtimereply@outlook.com' 
+
+
+
+AWS_STORAGE_BUCKET_NAME = "django-tech-blog"
+AWS_S3_REGION_NAME = "eu-west-2"
+AWS_ACCESS_KEY_ID = os.getenv('AWS_ACCESS_KEY_ID')
+AWS_SECRET_ACCESS_KEY = os.getenv('AWS_SECRET_ACCESS_KEY')
+
+STORAGES = {
+    "default": {
+        "BACKEND": "custom_storages.MediaFileStorage",
+    },
+    "staticfiles": {
+        "BACKEND": "custom_storages.StaticFileStorage",
+    }
+}
+
+AWS_S3_CUSTOM_DOMAIN = f"{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com"
+
+STATICFILES_FOLDER = "static"
+
+MEDIAFILES_FOLDER = "media"
+
+
+
